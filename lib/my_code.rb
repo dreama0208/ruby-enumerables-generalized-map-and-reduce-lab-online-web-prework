14b89a1ed33cc681e84b31968853ca596339505e
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-08 23:13:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,8 @@
 # Your Code Here
 def map(source_array)
   new = []
-  source_array.length { |i|
-    new.push (source_array[i])
+  source_array.length { |n|
+    new.push (source_array[n])
   }
   yield (new)
 end
